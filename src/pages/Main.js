35a5a034cd5162b3ee84e1165219a3f81dadca06
Christine Wong <jumiryoo@gmail.com>
2023-08-16 18:50:08 -0700

    frontend auth complete

---
+++ src/pages/Main.js
@@ -0,0 +1,15 @@
+import { Outlet, Link } from 'react-router-dom';
+
+function Main(props){
+    return <div>
+        <h1>Welcome to the People App</h1>
+        <h3>Do you want to sign up?</h3>
+        <Link to="/signup"><button>Sign Up</button></Link>
+        <h3>Do you want to log in?</h3>
+        <Link to="/login"><button>Log In</button></Link>
+        <Outlet />
+        </div>
+
+}
+
+export default Main;
